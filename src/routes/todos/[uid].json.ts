84c2cfd511e05abd13899104657648444d417534
Harmonions <me@harmonies.world>
2022-02-09 23:18:24 +0100

    Test cloudflare

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -1,16 +1,17 @@
 import { api } from './_api';
 import type { RequestHandler } from '@sveltejs/kit';
-import type { Locals } from '$lib/types';
 
 // PATCH /todos/:uid.json
-export const patch: RequestHandler<Locals, FormData> = async (request) => {
-	return api(request, `todos/${request.locals.userid}/${request.params.uid}`, {
-		text: request.body.get('text'),
-		done: request.body.has('done') ? !!request.body.get('done') : undefined
+export const patch: RequestHandler = async (event) => {
+	const data = await event.request.formData();
+
+	return api(event, `todos/${event.locals.userid}/${event.params.uid}`, {
+		text: data.get('text'),
+		done: data.has('done') ? !!data.get('done') : undefined
 	});
 };
 
 // DELETE /todos/:uid.json
-export const del: RequestHandler<Locals> = async (request) => {
-	return api(request, `todos/${request.locals.userid}/${request.params.uid}`);
+export const del: RequestHandler = async (event) => {
+	return api(event, `todos/${event.locals.userid}/${event.params.uid}`);
 };
