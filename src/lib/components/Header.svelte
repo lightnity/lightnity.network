31636e408b810d4f64bc562ca333c40d96e960b9
Harmonions <me@harmonies.world>
2022-02-17 17:44:55 +0100

    wip

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,6 +3,9 @@
 	import Lightnity from '$static/lightnity.svg';
 	import logoUrl from '$static/logo.svg?url';
 	import Icon from '@iconify/svelte';
+	import { fade, fly } from 'svelte/transition';
+
+	let active = false;
 </script>
 
 <header class="sticky top-0 z-50 flex items-center h-20">
@@ -12,17 +15,20 @@
 	</div>
 
 	<div class="grow" />
-	<div class="md:invisible mr-3"><Icon icon="mdi-light:home" /></div>
-	<ul class="invisible md:visible flex">
-		<li class="mr-6">
-			<a
-				sveltekit:prefetch
-				class="text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
-				href="/blog"
+
+	<div class="md:hidden mr-3">
+		{#if active == false}
+			<button class:active class="text-black dark:text-white" on:click={() => (active = !active)}
+				><Icon icon="charm:menu-hamburger" width="24" height="24" /></button
 			>
-				Learn
-			</a>
-		</li>
+		{:else}
+			<button class="text-black dark:text-white" on:click={() => (active = !active)}
+				><Icon icon="eva:close-fill" width="28" height="28" /></button
+			>
+		{/if}
+	</div>
+
+	<ul class="hidden md:flex">
 		<li class="mr-6">
 			<a
 				sveltekit:prefetch
@@ -79,3 +85,54 @@
 		</li>
 	</ul>
 </header>
+
+{#if active}
+	<div in:fade out:fade class="w-screen flex justify-end">
+		<div class="grow flex flex-wrap justify-center content-center">
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="https://github.com/lightnity"
+			>
+				<Icon icon="akar-icons:github-fill" width="40" height="40" />
+			</a>
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="https://discord.gg/afBXZdEHeh"
+			>
+				<Icon icon="akar-icons:chat-dots" width="40" height="40" />
+			</a>
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="https://twitter.com/lightnityn"
+			>
+				<Icon icon="akar-icons:twitter-fill" width="40" height="40" />
+			</a>
+		</div>
+		<div class="flex flex-col items-end">
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-3xl text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="/blog"
+			>
+				Docs
+			</a>
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-3xl text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="/blog"
+			>
+				FAQ
+			</a>
+			<a
+				sveltekit:prefetch
+				class="py-2 mr-8 text-3xl text-black hover:text-primary-light dark:text-white dark:hover:text-primary-dark"
+				href="/blog"
+			>
+				Blog
+			</a>
+		</div>
+	</div>
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<div class="min-h-screen" />
-<div class="h-screen text-white"></div>
+<div class="min-h-screen dark:text-white"><p>This is a test text for not sure</p></div>
+<div class="h-screen dark:text-white">
+	<p>This is a test text for not sure</p>
+</div>
